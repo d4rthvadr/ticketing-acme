373c91268941a088dc15c8212dab204bab3ad8a8
darkvadr <glowburn.acc@gmail.com>
2025-03-15 09:46:24 +0000

    fix types on asyncHandler

--- tickets/src/utils/async-handler.ts
+++ tickets/src/utils/async-handler.ts
@@ -1,3 +1,7 @@
-export const asyncHandler = fn => (req, res, next) => {
+import { NextFunction, Request, Response } from "express";
+
+export const asyncHandler =
+  (fn: (req: Request, res: Response, next: NextFunction) => Promise<unknown>) =>
+  (req: Request, res: Response, next: NextFunction) => {
     Promise.resolve(fn(req, res, next)).catch(next);
-}
+  };
