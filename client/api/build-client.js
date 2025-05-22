3d001ab2e2c204a45a9e12b8d9ac23b9107c40b9
darkvadr <glowburn.acc@gmail.com>
2025-05-22 09:06:39 +0000

    update loadbalancer link

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,21 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
 
-const loadBalancerName = "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local";
+// const loadBalancerName = "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local";
+const loadBalancerName = "http://vortex-app-vault.xyz";
 
 export default ({ req } = {}) => {
-
-  if (typeof window === 'undefined') {
+  if (typeof window === "undefined") {
     // on the server
     return axios.create({
       baseURL: loadBalancerName,
-      headers: req?.headers
+      headers: req?.headers,
     });
-
   } else {
     // client
     return axios.create({
-      baseURL: '/'
+      baseURL: "/",
     });
   }
-
-}
+};
